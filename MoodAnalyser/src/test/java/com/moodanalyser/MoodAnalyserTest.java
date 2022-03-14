8193b908adb970a8acde4fd2e00dc0c51269d7cd
deven7818 <deven7818@gmail.com>
2022-03-14 23:36:13 +0530

    respond sad mood

--- MoodAnalyser/src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ MoodAnalyser/src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -5,9 +5,8 @@ import org.junit.Test;
 
 /**
  * We have created this test method to check the mood.
- * If we provide the string of happy then the method should return happy
  * If we provide the string of sad then the method should return sad.
- * We have used the assert equals to check if the method o/p is happy or sad.
+ * We have used the assert equals to check if the method is sad.
  *
  */
 public class MoodAnalyserTest {
@@ -16,16 +15,6 @@ public class MoodAnalyserTest {
 	 */
 	MoodAnalyzer mood = new MoodAnalyzer();
 	
-	/**
-	 * method meeageHappyMood to check mood 
-	 * it returns Happy mood as we pass happy mood
-	 */
-	@Test
-	public void messageHappyMood() {
-		String result = mood.analyseMood("I am in Happy mood" );
-		Assert.assertEquals("HAPPY", result);
-	}
-
 	/**
 	 * method meeageSadMood to check mood 
 	 * it returns Sad mood as we pass sad mood
