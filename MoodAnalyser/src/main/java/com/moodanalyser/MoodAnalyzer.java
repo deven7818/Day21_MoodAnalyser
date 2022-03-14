1af54b0706b28f180bcf5b050350dee78b22ae7a
deven7818 <deven7818@gmail.com>
2022-03-14 23:55:05 +0530

    message in constructor

--- MoodAnalyser/src/main/java/com/moodanalyser/MoodAnalyzer.java
+++ MoodAnalyser/src/main/java/com/moodanalyser/MoodAnalyzer.java
@@ -6,8 +6,38 @@ package com.moodanalyser;
  * 
  * We Created this class to check the mood Happy , Sad or Any mood
  * We Created a method analyseMood that will take a string input and return the moods.
+ * Created default and parameterized constructor and passed message through constructor
  */
 public class MoodAnalyzer {
+	/*
+	 * String message variable 
+	 */
+	public String message;
+	
+	
+	/*
+	 * Default Constructor
+	 */
+	public MoodAnalyzer() {
+	}
+	/*
+	 * Parameterized Constructor
+	 */
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * Getter and setter methods
+	 * @return message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
 	/**
 	 * Method analyseMood check if the mood is happy or sad.
 	 * 1. We will convert the message to lower case and check it the string contains happy or sad word in it.
@@ -16,6 +46,8 @@ public class MoodAnalyzer {
 	 * @param message -  passing the string message from the method.
 	 * @return - will return the mood Happy or Sad 
 	 */
+	
+	
 	public String analyseMood(String message) {
 		if (message.toLowerCase().contains("happy")) {
 			return "HAPPY";
